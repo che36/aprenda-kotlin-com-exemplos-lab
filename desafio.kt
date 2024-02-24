1432c96423b5a00f809a3ea297f89682b0c9368c
Neo40 <tron36.cc@gmail.com>
2024-02-24 11:30:09 -0300

    Mudanças feitas para Lab de klotin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,28 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+    val dificuldade = Nivel.BASICO
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudos = listOf(
+            ConteudoEducacional("Conteúdo 1"),
+            ConteudoEducacional("Conteúdo 2"),
+            ConteudoEducacional("Conteúdo 3")
+    )
+
+    val formacao = Formacao("Formação Kotlin", conteudos)
+    val usuario = Usuario("Cesar")
+
+    formacao.matricular(usuario)
 }
